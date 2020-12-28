4ea3dff1d9a59f7aadf7e5517ddfc04bde7258f1
ziho00 <1253416625@qq.com>
2020-12-28 20:01:26 +0800

    设置 antd 组件文字使用中文

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="local">
+    <router-view />
+  </a-config-provider>
 </template>
 
 <script>
+import zhCN from "ant-design-vue/es/locale/zh_CN";
 export default {
   name: "App",
+  setup() {
+    return {
+      local: zhCN,
+    };
+  },
 };
 </script>
