0f0403fd7e168f283621efd2f01ac3d0cb2a3de8
ziho00 <1253416625@qq.com>
2021-01-16 14:33:41 +0800

    状态 Tag

---
+++ src/components/StateTag/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-tag :color="color" :style="style">{{ text }}</a-tag>
+</template>
+
+<script lang="ts">
+import { onBeforeMount, ref } from "vue";
+
+const COLOR = {
+  task: ["default", "blue", "green", "default"],
+};
+
+const TEXT = {
+  task: ["未开始", "进行中", "已完成", "已关闭"],
+};
+
+export default {
+  name: "StateTag",
+  props: {
+    mode: {
+      type: String,
+      default: "task",
+    },
+    value: {
+      type: String || Number,
+      default: 0,
+    },
+    style: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    const color = ref<string>("#d9d9d9");
+    const text = ref<string>("未开始");
+
+    onBeforeMount(() => {
+      color.value = COLOR[props.mode][props.value];
+      text.value = TEXT[props.mode][props.value];
+    });
+
+    return {
+      color,
+      text,
+    };
+  },
+};
+</script>
+
+<style></style>
