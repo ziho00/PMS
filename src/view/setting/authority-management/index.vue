769d38143a054abc22e8b0ce07dfa76712b4d949
ziho00 <1253416625@qq.com>
2021-01-18 14:56:28 +0800

    update page header component

--- src/view/setting/authority-management/index.vue
+++ src/view/setting/authority-management/index.vue
@@ -1,7 +1,34 @@
-<template></template>
+<template>
+  <a-page-header
+    style="border: 1px solid rgb(235, 237, 240)"
+    title="权限项管理"
+    :breadcrumb="{ routes }"
+  />
+  <a-card class="page-card"> 权限项管理 </a-card>
+</template>
 
-<script>
-export default {};
+<script lang="ts">
+const routes = [
+  {
+    path: "/",
+    breadcrumbName: "主页",
+  },
+  {
+    path: "/setting",
+    breadcrumbName: "设置",
+  },
+  {
+    path: "/role",
+    breadcrumbName: "权限项管理",
+  },
+];
+export default {
+  setup() {
+    return {
+      routes,
+    };
+  },
+};
 </script>
 
 <style></style>
--- src/view/setting/role-management/index.vue
+++ src/view/setting/role-management/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <a-card class="page-card"> role management </a-card>
+  <a-page-header
+    style="border: 1px solid rgb(235, 237, 240)"
+    title="角色管理"
+    :breadcrumb="{ routes }"
+  />
+  <a-card class="page-card"> 角色管理 </a-card>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+const routes = [
+  {
+    path: "/",
+    breadcrumbName: "主页",
+  },
+  {
+    path: "/setting",
+    breadcrumbName: "设置",
+  },
+  {
+    path: "/role",
+    breadcrumbName: "角色管理",
+  },
+];
+export default {
+  setup() {
+    return {
+      routes,
+    };
+  },
+};
 </script>
 
 <style></style>
