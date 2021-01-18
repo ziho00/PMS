b0c79387f91bf5e376152902686327fc962ced20
ziho00 <1253416625@qq.com>
2021-01-18 13:45:22 +0800

    QueryForm 组件

---
+++ src/components/QueryForm/QueryFormItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <a-col class="form-item" :md="9" :sm="24">
+    <a-row v-if="label">
+      <a-col :span="4" class="form-item-label">
+        <span>{{ label }}:</span>
+      </a-col>
+      <a-col :span="20">
+        <slot></slot>
+      </a-col>
+    </a-row>
+    <a-row v-else>
+      <a-col :span="24">
+        <slot></slot>
+      </a-col>
+    </a-row>
+  </a-col>
+</template>
+
+<script>
+export default {
+  name: "ZQueryFormItem",
+  props: {
+    label: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.form-item {
+  margin: 15px 0;
+}
+.form-item-label {
+  line-height: 32px;
+  padding-right: 15px;
+  width: auto;
+  text-align: right;
+}
+</style>
---
+++ src/components/QueryForm/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <a-row class="z-queryForm" :gutter="24">
+    <slot></slot>
+    <slot name="more" :show="showMore"></slot>
+    <a-col :md="6" :sm="24">
+      <div :style="{ padding: '15px 0' }">
+        <a-button :loading="searching" type="primary" @click="onSearch"
+          >查询</a-button
+        >
+        <a-button
+          :loading="reseting"
+          :style="{ marginLeft: '15px' }"
+          @click="onReset"
+          >重置</a-button
+        >
+        <a-button type="link" @click="toggleShowMore">
+          {{ showMore ? "收起" : "展开" }}
+          <DownOutlined v-show="!showMore" />
+          <UpOutlined v-show="showMore"
+        /></a-button>
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script lang="ts">
+import { getCurrentInstance, ref } from "vue";
+import { DownOutlined, UpOutlined } from "@ant-design/icons-vue";
+export default {
+  name: "ZQueryFrom",
+  components: {
+    DownOutlined,
+    UpOutlined,
+  },
+  props: {
+    model: {
+      type: Object,
+      isRequire: true,
+    },
+    searching: {
+      type: Boolean,
+      default: false,
+    },
+    reseting: {
+      type: Boolean,
+      default: false,
+    },
+    more: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const vm = getCurrentInstance();
+    const showMore = ref<boolean>(false);
+
+    const onSearch = () => {
+      vm.emit("onSearch", props.model);
+    };
+
+    const onReset = () => {
+      vm.emit("onReset");
+    };
+
+    const toggleShowMore = () => {
+      showMore.value = !showMore.value;
+      vm.emit("toggleShowMore", showMore.value);
+    };
+
+    return {
+      onSearch,
+      onReset,
+      showMore,
+      toggleShowMore,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.z-queryForm {
+}
+</style>
