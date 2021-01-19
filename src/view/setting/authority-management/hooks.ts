d3b523b9792322fb8f168154a2ab80450fe0856f
ziho00 <1253416625@qq.com>
2021-01-19 12:32:10 +0800

    update: 权限管理

---
+++ src/view/setting/authority-management/hooks.ts
@@ -0,0 +1,120 @@
+import { reactive, ref } from "vue";
+import { api } from "/@/http/api";
+
+function useQueryForm() {
+  const queryLoading = reactive({ search: false, reset: false });
+  const queryFormData = reactive({ name: "" });
+
+  return {
+    queryFormData,
+    queryLoading,
+  };
+}
+
+function useTable(queryFormData) {
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "authority_id",
+      key: "authority_id",
+    },
+    {
+      title: "权限项名称",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "权限项秘钥",
+      dataIndex: "token",
+      key: "token",
+      slots: { customRender: "token" },
+    },
+    {
+      title: "创建时间",
+      dataIndex: "createTime",
+      key: "createTime",
+    },
+    {
+      title: "操作",
+      dataIndex: "operation",
+      slots: { customRender: "operation" },
+    },
+  ];
+  const dataSource = ref<Array<any>>([]);
+  const selectedRowKeys = ref<Array<any>>([]);
+  const loading = ref<boolean>(false);
+  const pagination = reactive({
+    current: 1,
+    pageSize: 10,
+    defaultPageSize: 10,
+    total: 0,
+    showSizeChanger: true,
+  });
+
+  const onSelectChange = (selected) => {
+    selectedRowKeys.value = selected;
+  };
+
+  const handleTableChange = (pager) => {
+    pagination.current = pager.current;
+    pagination.pageSize = pager.pageSize;
+    refresh();
+  };
+
+  const refresh = async (isToFirstPage?: boolean) => {
+    console.log(queryFormData, pagination);
+    if (isToFirstPage) {
+      pagination.current = 1;
+    }
+    const res = await api.authorityManagement.getAuthoritys();
+    dataSource.value = res.data.data;
+  };
+
+  return {
+    columns,
+    dataSource,
+    loading,
+    pagination,
+    selectedRowKeys,
+    onSelectChange,
+    handleTableChange,
+    refresh,
+  };
+}
+
+function useModal() {
+  const visible = ref<boolean>(false);
+  const confirmLoading = ref<boolean>(false);
+  const isEdit = ref<boolean>(false);
+  const selectorLoading = ref<boolean>(false);
+  const authorityItem = reactive({
+    authority_id: "",
+    name: "",
+    token: "",
+  });
+
+  // 提交编辑
+  const handleSubmit = () => {
+    console.log(authorityItem);
+    if (isEdit) {
+      // todo...
+    } else {
+      // todo...
+    }
+  };
+
+  return {
+    visible,
+    confirmLoading,
+    selectorLoading,
+    authorityItem,
+    isEdit,
+    handleSubmit,
+  };
+}
+
+function useForm() {
+  return {};
+}
+
+export { useForm, useModal, useQueryForm, useTable };
--- src/view/setting/authority-management/index.vue
+++ src/view/setting/authority-management/index.vue
@@ -4,10 +4,106 @@
     title="权限项管理"
     :breadcrumb="{ routes }"
   />
-  <a-card class="page-card"> 权限项管理 </a-card>
+  <a-card class="page-card">
+    <!-- query form -->
+    <z-query-form
+      :model="queryFormData"
+      @onSearch="handleSearch"
+      @onReset="handleReset"
+      :searching="queryLoading.search"
+      :reseting="queryLoading.reset"
+    >
+      <z-query-form-item label="权限项名称">
+        <a-input v-model:value="queryFormData.name" placeholder="权限项名称" />
+      </z-query-form-item>
+    </z-query-form>
+
+    <!-- create & delete operator -->
+    <div class="table-operator">
+      <a-button @click="handleCreateAuthorytyItem" type="primary"
+        ><PlusOutlined /> 创建</a-button
+      >
+      <a-button
+        @click="handleDeleteAuthorytyItems"
+        :style="{ marginLeft: '24px' }"
+        type="danger"
+      >
+        删除</a-button
+      >
+    </div>
+
+    <!-- table -->
+    <a-table
+      :columns="columns"
+      :data-source="dataSource"
+      :pagination="pagination"
+      :loading="loading"
+      rowKey="authority_id"
+      @change="handleTableChange"
+      :row-selection="{
+        selectedRowKeys: selectedRowKeys,
+        onChange: onSelectChange,
+      }"
+    >
+      <template #token="{ text }">
+        <a-tag>{{ text }}</a-tag>
+      </template>
+      <template #operation="{ record }">
+        <a-button type="link" @click="handleEdit(record)">编辑</a-button>
+        <span style="color: var(--remark-color)">|</span>
+        <a-popconfirm
+          title="确认删除？"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="handleDeleteAuthorytyItem(record)"
+        >
+          <a-button type="link"> 删除 </a-button>
+        </a-popconfirm>
+      </template>
+    </a-table>
+  </a-card>
+
+  <!-- 创建/编辑 Form -->
+  <a-modal
+    :title="
+      isEdit ? `编辑权限项【${authorityItem.authority_id}】` : '创建权限项'
+    "
+    width="500px"
+    v-model:visible="visible"
+    :confirm-loading="confirmLoading"
+    @ok="handleSubmit"
+  >
+    <a-form
+      ref="userForm"
+      :model="authorityItem"
+      :label-col="{ span: 5 }"
+      :wrapper-col="{ span: 16 }"
+    >
+      <a-form-item v-if="isEdit" label="ID" name="user_id">
+        <a-input v-model:value="authorityItem.authority_id" disabled />
+      </a-form-item>
+      <a-form-item label="名称" name="name">
+        <a-input v-model:value="authorityItem.name" placeholder="权限项名称" />
+      </a-form-item>
+      <a-form-item label="秘钥" name="token">
+        <a-input v-model:value="authorityItem.token" placeholder="权限项秘钥" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
 <script lang="ts">
+import { reactive, ref, onBeforeMount, createVNode } from "vue";
+import ZQueryForm from "/@/components/QueryForm/index.vue";
+import ZQueryFormItem from "/@/components/QueryForm/QueryFormItem.vue";
+import { api } from "/@/http/api";
+import { useForm, useModal, useQueryForm, useTable } from "./hooks";
+import { Modal } from "ant-design-vue";
+import {
+  PlusOutlined,
+  DeleteOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons-vue";
 const routes = [
   {
     path: "/",
@@ -23,9 +119,120 @@ const routes = [
   },
 ];
 export default {
+  name: "AuthorityManagement",
+  components: {
+    ZQueryForm,
+    ZQueryFormItem,
+    PlusOutlined,
+    DeleteOutlined,
+  },
   setup() {
+    const { queryFormData, queryLoading } = useQueryForm();
+
+    const {
+      columns,
+      dataSource,
+      loading,
+      pagination,
+      selectedRowKeys,
+      onSelectChange,
+      handleTableChange,
+      refresh,
+    } = useTable(queryFormData);
+
+    const {
+      visible,
+      confirmLoading,
+      selectorLoading,
+      authorityItem,
+      isEdit,
+      handleSubmit,
+    } = useModal();
+
+    const {} = useForm();
+
+    onBeforeMount(async () => {
+      await refresh();
+    });
+
+    // 查询项目
+    const handleSearch = (formData) => {
+      queryLoading.search = true;
+      console.log(formData);
+      setTimeout(() => {
+        queryLoading.search = false;
+      }, 1000);
+    };
+
+    // 重置搜索表单数据
+    const handleReset = () => {
+      queryLoading.reset = true;
+      queryFormData.name = "";
+      console.log(queryFormData);
+      setTimeout(() => {
+        queryLoading.reset = false;
+      }, 1000);
+    };
+
+    // 创建
+    const handleCreateAuthorytyItem = () => {
+      Object.keys(authorityItem).map((key) => {
+        authorityItem[key] = "";
+      });
+      visible.value = true;
+      isEdit.value = false;
+    };
+
+    // 批量删除
+    const handleDeleteAuthorytyItems = () => {
+      Modal.confirm({
+        title: "确定删除选中的权限项?",
+        icon: createVNode(ExclamationCircleOutlined),
+        onOk: async () => {
+          console.log(selectedRowKeys.value);
+        },
+      });
+    };
+
+    // 单独删除
+    const handleDeleteAuthorytyItem = (record) => {
+      console.log(record);
+      // todo
+    };
+
+    // 点击编辑
+    const handleEdit = (record) => {
+      isEdit.value = true;
+      Object.keys(authorityItem).map((key) => {
+        authorityItem[key] = record[key];
+      });
+      visible.value = true;
+    };
+
     return {
       routes,
+      queryFormData,
+      queryLoading,
+      handleSearch,
+      handleReset,
+      columns,
+      dataSource,
+      loading,
+      pagination,
+      handleTableChange,
+      refresh,
+      selectedRowKeys,
+      onSelectChange,
+      handleCreateAuthorytyItem,
+      handleEdit,
+      handleDeleteAuthorytyItems,
+      handleDeleteAuthorytyItem,
+      visible,
+      confirmLoading,
+      selectorLoading,
+      authorityItem,
+      isEdit,
+      handleSubmit,
     };
   },
 };
