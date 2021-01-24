9cae1a238a9b3bb325983a9c8bf9a2a333c4d09e
ziho00 <1253416625@qq.com>
2021-01-24 12:54:10 +0800

    no message

--- src/http/api/index.ts
+++ src/http/api/index.ts
@@ -1,14 +1,31 @@
 import * as user from "./user";
 import * as workplace from "./workplace";
-import * as project from "./project";
+
+/** setting  */
 import * as userManagement from "./user-management";
 import * as roleManagement from "./role-management";
 import * as authorityManagement from "./authority-management";
+
+/** project */
+import * as project from "./project";
+import * as task from "./task";
+import * as requirement from "./requirement";
+import * as bug from "./bug";
+import * as version from "./version";
+import * as charts from "./charts";
+import * as projectSetting from "./project_setting";
+
 export const api = {
   user,
   workplace,
-  project,
   userManagement,
   roleManagement,
   authorityManagement,
+  project,
+  task,
+  requirement,
+  bug,
+  version,
+  charts,
+  projectSetting,
 };
