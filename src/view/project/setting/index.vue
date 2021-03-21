49e7c21a45b80eda132263fbb6efb9c20cb1a720
ziho00 <1253416625@qq.com>
2021-03-21 13:53:01 +0800

    dev: project settings module

--- src/view/project/setting/index.vue
+++ src/view/project/setting/index.vue
@@ -1,7 +1,51 @@
-<template></template>
+<template>
+  <a-card class="page-card" :bodyStyle="{ padding: '16px 0' }">
+    <div class="project-settings-main">
+      <a-menu
+        :style="{ width: '224px', minHeight: '400px' }"
+        v-model:openKeys="openKeys"
+        v-model:selectedKeys="selectedKeys"
+        mode="inline"
+      >
+        <a-menu-item key="info">项目信息</a-menu-item>
+        <a-menu-item key="member">项目成员</a-menu-item>
+      </a-menu>
+      <div class="project-settings-container">
+        <Info v-if="selectedKeys.includes('info')" />
+        <Member v-if="selectedKeys.includes('member')" />
+      </div>
+    </div>
+  </a-card>
+</template>
 
 <script lang="ts">
-export default {};
+import { ref } from "vue";
+import Info from "./info/index.vue";
+import Member from "./members/index.vue";
+
+export default {
+  components: {
+    Info,
+    Member,
+  },
+  setup() {
+    const openKeys = ref([]);
+    const selectedKeys = ref(["info"]);
+
+    return {
+      openKeys,
+      selectedKeys,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.project-settings-main {
+  display: flex;
+  .project-settings-container {
+    padding: 0 10px;
+    width: calc(100% - 224px);
+  }
+}
+</style>
