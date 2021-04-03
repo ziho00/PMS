05b6e732274a3ed5ea802c87711ca5273311aa9a
ziho00 <1253416625@qq.com>
2021-04-03 09:43:26 +0800

    dev: charts module

--- src/view/project/charts/index.vue
+++ src/view/project/charts/index.vue
@@ -1,7 +1,27 @@
-<template></template>
+<template>
+  <a-card class="page-card" :bodyStyle="{ padding: '16px 0' }">
+    <a-menu
+      :style="{ width: '224px', minHeight: '400px' }"
+      v-model:selectedKeys="selectedKeys"
+      mode="inline"
+    >
+      <a-menu-item key="task">任务报表</a-menu-item>
+      <a-menu-item key="requirement">需求报表</a-menu-item>
+      <a-menu-item key="bug">缺陷报表</a-menu-item>
+    </a-menu>
+  </a-card>
+</template>
 
 <script lang="ts">
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const selectedKeys = ref(["task"]);
+    return {
+      selectedKeys,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
